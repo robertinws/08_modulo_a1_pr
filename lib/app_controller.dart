ed96f00fd294779d840b3b8f174e9ed7db163bcf
robertinws <bertinws@gmail.com>
2025-08-05 09:09:14 -0300

    Controller criado

---
+++ lib/app_controller.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:modulo_a1_pr/pages/loading_page.dart';
+
+class AppController extends StatelessWidget {
+  const AppController({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {'/': (context) => LoadingPage()},
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,6 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:modulo_a1_pr/app_controller.dart';
+
+void main() {
+  runApp(AppController());
+}
